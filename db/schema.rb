383fc012e27d34c646357f0efd64f7f65964d943
ryoji369 <ryoji570510@icloud.com>
2021-04-07 11:29:20 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_100157) do
+ActiveRecord::Schema.define(version: 2021_04_07_022559) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
